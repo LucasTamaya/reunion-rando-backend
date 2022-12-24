884dd087a584a2ab3cf923a50656367b92923554
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-24 10:28:23 +0400

    updated start script

--- dist/app.js
+++ dist/app.js
@@ -7,19 +7,25 @@ import { router as authRoutes } from "./routes/authRoutes.js";
 import { router as userRoutes } from "./routes/userRoutes.js";
 import { router as hikeRoutes } from "./routes/hikeRoutes.js";
 import { router as activityRoutes } from "./routes/activityRoutes.js";
+const productionMode = process.env.NODE_ENV === "production";
 export const createServer = () => {
-  const app = express();
-  // server config
-  app.use(bodyParser.json());
-  app.use(cookieParser());
-  app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" })); // origin a modifier pour production
-  // add 15 small middlewares to protect the app
-  app.use(helmet());
-  // api routes
-  app.use(authRoutes);
-  app.use(userRoutes);
-  app.use(hikeRoutes);
-  app.use(activityRoutes);
-  return app;
+    const app = express();
+    // server config
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(cors({
+        credentials: true,
+        origin: productionMode
+            ? "https://reunion-rando.vercel.app/"
+            : "http://localhost/",
+    })); // origin a modifier pour production
+    // add 15 small middlewares to protect the app
+    app.use(helmet());
+    // api routes
+    app.use(authRoutes);
+    app.use(userRoutes);
+    app.use(hikeRoutes);
+    app.use(activityRoutes);
+    return app;
 };
 //# sourceMappingURL=app.js.map
