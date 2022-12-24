229bdca2d915ee5a4b37db7b4409c298b56a2f94
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-24 10:15:27 +0400

    added dist folder to .gitignore

--- dist/app.js
+++ dist/app.js
@@ -8,18 +8,18 @@ import { router as userRoutes } from "./routes/userRoutes.js";
 import { router as hikeRoutes } from "./routes/hikeRoutes.js";
 import { router as activityRoutes } from "./routes/activityRoutes.js";
 export const createServer = () => {
-    const app = express();
-    // server config
-    app.use(bodyParser.json());
-    app.use(cookieParser());
-    app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" })); // origin a modifier pour production
-    // add 15 small middlewares to protect the app
-    app.use(helmet());
-    // api routes
-    app.use(authRoutes);
-    app.use(userRoutes);
-    app.use(hikeRoutes);
-    app.use(activityRoutes);
-    return app;
+  const app = express();
+  // server config
+  app.use(bodyParser.json());
+  app.use(cookieParser());
+  app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" })); // origin a modifier pour production
+  // add 15 small middlewares to protect the app
+  app.use(helmet());
+  // api routes
+  app.use(authRoutes);
+  app.use(userRoutes);
+  app.use(hikeRoutes);
+  app.use(activityRoutes);
+  return app;
 };
 //# sourceMappingURL=app.js.map
