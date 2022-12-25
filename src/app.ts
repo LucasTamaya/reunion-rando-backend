5d4e1cd098c658f9fdaef85dc51a1393733147e2
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-25 16:28:09 +0400

    updated cors origin urls with an array

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,12 @@ import { router as userRoutes } from "./routes/userRoutes.js";
 import { router as hikeRoutes } from "./routes/hikeRoutes.js";
 import { router as activityRoutes } from "./routes/activityRoutes.js";
 
-const productionMode = process.env.NODE_ENV === "production";
+const allowedUrls = [
+  "http://127.0.0.1:5173",
+  "https://reunion-rando.vercel.app",
+  "https://reunion-rando-lucastamaya.vercel.app",
+  "https://reunion-rando-git-master-lucastamaya.vercel.app",
+];
 
 export const createServer = () => {
   const app = express();
@@ -20,9 +25,7 @@ export const createServer = () => {
   app.use(
     cors({
       credentials: true,
-      origin: productionMode
-        ? "https://reunion-rando.vercel.app"
-        : "http://localhost",
+      origin: allowedUrls,
     })
   );
   // add 15 small middlewares to protect the app
